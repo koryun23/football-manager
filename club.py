1da8463f0566269e14d527aeca85359f67ec90a5
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-12-24 22:27:54 +0400

    making minor changes to the Player class

---
+++ club.py
@@ -0,0 +1,10 @@
+from player import Player
+
+
+class Club:
+    def __init__(self, name, budget, player_list):
+        self.name = name
+        self.budget = budget
+        self.player_list = player_list
+
+
