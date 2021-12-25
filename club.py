3a65e75e74d285cc61549f3c4b67ff8e902a2920
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-12-25 16:57:49 +0400

    getting the players on each position sorted in ascending order of their skills

--- club.py
+++ club.py
@@ -2,17 +2,71 @@ from player import Player
 
 
 class Club:
+    formation_hierarchy = [[4, 3, 3], [4, 2, 3, 1], [4, 4, 2], [3, 2, 3, 2]]
+
     def __init__(self, name, budget, player_list):
         self.name = name
         self.budget = budget
         self.player_list = player_list
         self.formation = []
+
     def player_list_for_game(self):
-        list = [self.player_list[0]]
+        gk = [self.player_list[0]]
+        cbs = []
+        lbs = []
+        rbs = []
+        dms = []
+        cms = []
+        lms = []
+        rms = []
+        lws = []
+        rws = []
+        cfs = []
         for player in self.player_list:
             if player["position"] == "gk":
-                if player["skill"] > list[0]["skill"]:
-                    list[0] = player
-            else:
-                break
-        return list
+                if player["skill"] > gk[0]["skill"]:
+                    gk[0] = player
+            elif player["position"] == "cb":
+                cbs.append(player)
+            elif player["position"] == "lb":
+                lbs.append(player)
+            elif player["position"] == "rb":
+                rbs.append(player)
+            elif player["position"] == "dm":
+                dms.append(player)
+            elif player["position"] == "cm":
+                cms.append(player)
+            elif player["position"] == "lm":
+                lms.append(player)
+            elif player["position"] == "rm":
+                rms.append(player)
+            elif player["position"] == "lw":
+                lws.append(player)
+            elif player["position"] == "rw":
+                rws.append(player)
+            elif player["position"] == "cf":
+                cfs.append(player)
+
+
+        cbs = sorted(cbs, key=lambda current_player: current_player["skill"])
+        lbs = sorted(lbs, key=lambda current_player: current_player["skill"])
+        rbs = sorted(rbs, key=lambda current_player: current_player["skill"])
+        dms = sorted(dms, key=lambda current_player: current_player["skill"])
+        cms = sorted(cms, key=lambda current_player: current_player["skill"])
+        lms = sorted(lms, key=lambda current_player: current_player["skill"])
+        rms = sorted(rms, key=lambda current_player: current_player["skill"])
+        lws = sorted(lws, key=lambda current_player: current_player["skill"])
+        rws = sorted(rws, key=lambda current_player: current_player["skill"])
+        cfs = sorted(cfs, key=lambda current_player: current_player["skill"])
+        print(cbs)
+        print(lbs)
+        print(rbs)
+        print(dms)
+        print(cms)
+        print(lms)
+        print(rms)
+        print(lws)
+        print(rws)
+        print(cfs)
+
+
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from club import Club
 from tournament import Tournament
 from load_data import *
 
-print(team1.player_list_for_game())
-print(team2.player_list_for_game())
-class Game:
-    def __init__(self, team1, team2):
-        self.team1 = team1
-        self.team2 = team2
-    def formations(self, team):
-        pass
+team1.player_list_for_game()
+team2.player_list_for_game()
+# class Game:
+#     def __init__(self, team1, team2):
+#         self.team1 = team1
+#         self.team2 = team2
+#     def formations(self, team):
+#         pass
