f63530273e3df9aeb0a11e1bc88bcbcae365f9cb
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-12-25 22:17:06 +0400

    adding the 4-2-3-1 and 3-5-2 formations

--- club.py
+++ club.py
@@ -80,8 +80,8 @@ class Club:
 
             squad.append(cfs[0]["name"])
 
-        possible_squads.append(squad)
-        squad = [gk[0]["name"]]
+            possible_squads.append(squad)
+            squad = [gk[0]["name"]]
         if lbs and rbs and len(cbs) >= 2 and len(cms)+len(dms) == 2 and (len(lws) or len(lms)) and (len(rws) or len(rms)) and len(cfs) >= 2:
             squad.append(lbs[0]["name"])
             squad.append(rbs[0]["name"])
@@ -111,7 +111,51 @@ class Club:
             squad.append(cfs[0]["name"])
             squad.append(cfs[1]["name"])
             possible_squads.append(squad)
-            squad = []
+            squad = [gk[0]]
+        if len(cbs) >= 3 and (lms or lws) and (rms or rws) and len(cms) >= 3 and len(cfs)>=2:
+            squad.append(cbs[0]["name"])
+            squad.append(cbs[1]["name"])
+            squad.append(cbs[2]["name"])
+            if lms and lws:
+                if lms[0]["skill"] > lws[0]["skill"]:
+                    squad.append(lms[0]["name"])
+                else:
+                    squad.append(lws[0]["name"])
+            else:
+                if lms:
+                    squad.append(lms[0]["name"])
+                else:
+                    squad.append(lws[0]["name"])
+            if rms and rws:
+                if rms[0]["skill"] > rws[0]["skill"]:
+                    squad.append(rms[0]["name"])
+                else:
+                    squad.append(rws[0]["name"])
+            else:
+                if rms:
+                    squad.append(rms[0]["name"])
+                else:
+                    squad.append(rws[0]["name"])
+            squad.append(cms[0]["name"])
+            squad.append(cms[1]["name"])
+            squad.append(cms[2]["name"])
+            squad.append(cfs[0]["name"])
+            squad.append(cfs[1]["name"])
+            possible_squads.append(squad)
+            squad = [gk[0]]
+        if lbs and rbs and len(cbs) >= 2 and len(dms) >= 2 and lws and rws and ams and cfs:
+            squad.append(lbs[0]["name"])
+            squad.append(rbs[0]["name"])
+            squad.append(cbs[0]["name"])
+            squad.append(cbs[1]["name"])
+            squad.append(dms[0]["name"])
+            squad.append(dms[1]["name"])
+            squad.append(lws[0]["name"])
+            squad.append(rws[0]["name"])
+            squad.append(ams[0]["name"])
+            squad.append(cfs[0]["name"])
+            possible_squads.append(squad)
+            squad = [gk[0]]
         for sq in possible_squads:
             print(sq)
 
