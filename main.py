102c8e98d90739174a1121b3e8e96ab81eaa82d8
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-12-25 22:30:59 +0400

    removing comments

--- main.py
+++ main.py
@@ -6,9 +6,3 @@ from load_data import *
 
 team1.player_list_for_game()
 team2.player_list_for_game()
-# class Game:
-#     def __init__(self, team1, team2):
-#         self.team1 = team1
-#         self.team2 = team2
-#     def formations(self, team):
-#         pass
