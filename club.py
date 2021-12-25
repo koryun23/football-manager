a28aced9423fa3545a00916cc4f4cb4c8a6126db
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-12-25 20:48:35 +0400

    making some improvements and starting the implementation of method of getting the possible squads of a team for the upcoming game

--- club.py
+++ club.py
@@ -2,7 +2,7 @@ from player import Player
 
 
 class Club:
-    formation_hierarchy = [[4, 3, 3], [4, 2, 3, 1], [4, 4, 2], [3, 2, 3, 2]]
+    formation_hierarchy = [[4, 3, 3], [4, 2, 3, 1], [4, 4, 2], [3, 5, 2]] #probably won't need
 
     def __init__(self, name, budget, player_list):
         self.name = name
@@ -11,6 +11,8 @@ class Club:
         self.formation = []
 
     def player_list_for_game(self):
+        squad = []
+        possible_squads = []
         gk = [self.player_list[0]]
         cbs = []
         lbs = []
@@ -30,7 +32,7 @@ class Club:
                     gk[0] = player
             else:
                 d[player["position"]].append(player)
-
+        squad = [gk[0]["name"]]
         cbs = sorted(cbs, key=lambda current_player: -1*current_player["skill"])
         lbs = sorted(lbs, key=lambda current_player: -1*current_player["skill"])
         rbs = sorted(rbs, key=lambda current_player: -1*current_player["skill"])
@@ -42,16 +44,74 @@ class Club:
         lws = sorted(lws, key=lambda current_player: -1*current_player["skill"])
         rws = sorted(rws, key=lambda current_player: -1*current_player["skill"])
         cfs = sorted(cfs, key=lambda current_player: -1*current_player["skill"])
-        print(cbs)
-        print(lbs)
-        print(rbs)
-        print(dms)
-        print(cms)
-        print(lms)
-        print(ams)
-        print(rms)
-        print(lws)
-        print(rws)
-        print(cfs)
+        if lbs and rbs and len(cbs)>=2 and (len(cms) > 2 or (len(cms) == 2 and len(dms) >=1)) and len(cfs) and (len(lws) or len(lms)) and (len(rws) or len(rms)):
+            squad.append(lbs[0]["name"])
+            squad.append(rbs[0]["name"])
+            squad.append(cbs[0]["name"])
+            squad.append(cbs[1]["name"])
+            squad.append(cms[0]["name"])
+            squad.append(cms[1]["name"])
+            if not dms:
+                squad.append(cms[2]["name"])
+            else:
+                squad.append(dms[0]["name"])
 
+            if lws and lms:
+                if lms[0]["skill"] > lws[0]["skill"]:
+                    squad.append(lms[0]["name"])
+                else:
+                    squad.append(lws[0]["name"])
+            else:
+                if lws:
+                    squad.append(lws[0]["name"])
+                else:
+                    squad.append(lms[0]["name"])
+
+            if rws and rms:
+                if rms[0]["skill"] > rws[0]["skill"]:
+                    squad.append(rms[0]["name"])
+                else:
+                    squad.append(rws[0]["name"])
+            else:
+                if rws:
+                    squad.append(rws[0]["name"])
+                else:
+                    squad.append(rms[0]["name"])
+
+            squad.append(cfs[0]["name"])
+
+        possible_squads.append(squad)
+        squad = [gk[0]["name"]]
+        if lbs and rbs and len(cbs) >= 2 and len(cms)+len(dms) == 2 and (len(lws) or len(lms)) and (len(rws) or len(rms)) and len(cfs) >= 2:
+            squad.append(lbs[0]["name"])
+            squad.append(rbs[0]["name"])
+            squad.append(cbs[0]["name"])
+            squad.append(cbs[1]["name"])
+            if lws and lms:
+                if lws[0]["skill"] > lms[0]["skill"]:
+                    squad.append(lws[0]["name"])
+                else:
+                    squad.append(lms[0]["name"])
+            else:
+                if lws:
+                    squad.append(lws[0]["name"])
+                else:
+                    squad.append(lms[0]["name"])
+
+            if rws and rms:
+                if rws[0]["skill"] > rms[0]["skill"]:
+                    squad.append(rws[0]["name"])
+                else:
+                    squad.append(rms[0]["name"])
+            else:
+                if rws:
+                    squad.append(rws[0]["name"])
+                else:
+                    squad.append(rms[0]["name"])
+            squad.append(cfs[0]["name"])
+            squad.append(cfs[1]["name"])
+            possible_squads.append(squad)
+            squad = []
+        for sq in possible_squads:
+            print(sq)
 
