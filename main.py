069df1f9608d4de40e7ef64c4dc8ff491259c977
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-11 21:41:21 +0400

    adding graphics to see what happens on the pitch

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import time
+from tkinter import *
 from game import Game
 from player import Player
 from tournament import Tournament
 from club import Club
 from load_data import *
+
+tk = Tk()
+myCanvas = Canvas(tk, width=500, height=400)
+myCanvas.pack()
+
+
+def create_circle(x, y, r, canvas_name, fill):  # center coordinates, radius
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvas_name.create_oval(x0, y0, x1, y1, fill=fill)
 
 
 team1_squad = team1.player_list_for_game()[0]
@@ -32,5 +45,15 @@ for pl in team2_squad:
 print("\n\n\n")
 print("Game starts!")
 while game.minute <= 45:
+    for pl in game.team1.best_squad:
+        current_position_x = pl.pos_x
+        current_position_y = pl.pos_y
+        create_circle(5*current_position_x, 8*(50-current_position_y), 10, myCanvas, "lightblue")
+    for pl in game.team2.best_squad:
+        current_position_x = pl.pos_x
+        current_position_y = pl.pos_y
+        create_circle(5*current_position_x, 8*(50-current_position_y), 10, myCanvas, "red")
+    create_circle(5*game.ball_pos_x, 8*(50-game.ball_pos_y), 5, myCanvas, "white")
     game.play()
     time.sleep(1)
+    myCanvas.mainloop()
