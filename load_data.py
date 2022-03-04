d3fec034b0a311c86b916d0eb1d9bdda9b9d2f51
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-04 17:52:29 +0400

    breaking down the code to smalled methods

--- load_data.py
+++ load_data.py
@@ -3,10 +3,11 @@ from player import Player
 from club import Club
 from tournament import Tournament
 
+
 def filter_club_name(club):
     new_name = ""
     for i in range(len(club)):
-        if i == 0 or club[i-1] == "-":
+        if i == 0 or club[i - 1] == "-":
             new_name += club[i].upper()
         elif club[i] == "-":
             new_name += " "
@@ -14,44 +15,61 @@ def filter_club_name(club):
             new_name += club[i]
     return new_name
 
-f = open("data.json")
-data = json.load(f)
-premier_league_clubs = data["clubs"]
-club_names = ["manchester-city", "liverpool", "manchester-united", "chelsea", "tottenham-hotspur", "leicester-city", "arsenal", "west-ham-united",
-              "everton", "wolverhampton-wanderers", "aston-villa", "leeds-united", "newcastle-united", "southampton", "burnley", "brighton-hove-albion",
-              "watford", "norwich-city", "brentford", "crystal-palace"
-              ]
-clubs = []
-for club in club_names:
-    clubs.append(Club(filter_club_name(club), [], ""))
-epl = Tournament("Premier League", clubs)
-epl.generate_standings()
-epl.generate_pairings()
-epl.format_pairings()
-epl.print_pairings()
-# epl.print_clubs()
-team1 = epl.clubs[0]
-team2 = epl.clubs[1]
-for club in premier_league_clubs:
-    if club["name"] == team1.name:
-        pl_list = club["player_list"]
-        for player in pl_list:
-            new_player = Player(
-                player["name"],
-                player["age"],
-                player["skill"],
-                player["club"],
-                player["position"]
-            )
-            team1.player_list.append(new_player)
-    elif club["name"] == team2.name:
-        pl_list = club["player_list"]
-        for player in pl_list:
-            new_player = Player(
-                player["name"],
-                player["age"],
-                player["skill"],
-                player["club"],
-                player["position"]
-            )
-            team2.player_list.append(new_player)
+
+def load_teams_from_json(tournament_name):
+    f = open("data.json")
+    data = json.load(f)
+    league_clubs = data[tournament_name]["clubs"]
+    print(league_clubs)
+    return league_clubs
+
+
+def get_clubs_array():
+    club_names = ["manchester-city", "liverpool", "manchester-united", "chelsea", "tottenham-hotspur", "leicester-city",
+                  "arsenal", "west-ham-united",
+                  "everton", "wolverhampton-wanderers", "aston-villa", "leeds-united", "newcastle-united",
+                  "southampton", "burnley", "brighton-hove-albion",
+                  "watford", "norwich-city", "brentford", "crystal-palace"
+                  ]
+    clubs = []
+    for club in club_names:
+        clubs.append(Club(filter_club_name(club), [], ""))
+    return clubs
+
+
+def load_league(tournament_name):  # must return an array containing all Club objects in the current tournament
+    league = Tournament(tournament_name, get_clubs_array())
+    league.generate_standings()
+    league.generate_pairings()
+    league.format_pairings()
+    return league
+
+
+def load_teams(team1_name, team2_name):
+    league_clubs = load_teams_from_json("Premier League")
+    team1 = Club(team1_name, [], "")
+    team2 = Club(team2_name, [], "")
+    for club in league_clubs:
+        if club["name"] == team1_name:
+            pl_list = club["player_list"]
+            for player in pl_list:
+                new_player = Player(
+                    player["name"],
+                    player["age"],
+                    player["skill"],
+                    player["club"],
+                    player["position"]
+                )
+                team1.player_list.append(new_player)
+        elif club["name"] == team2_name:
+            pl_list = club["player_list"]
+            for player in pl_list:
+                new_player = Player(
+                    player["name"],
+                    player["age"],
+                    player["skill"],
+                    player["club"],
+                    player["position"]
+                )
+                team2.player_list.append(new_player)
+    return team1, team2
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import time
 from tkinter import *
 from game import Game
-from player import Player
-from tournament import Tournament
-from club import Club
 from threading import Thread
 from load_data import *
 
 tk = Tk()
 myCanvas = Canvas(tk, width=500, height=400)
 myCanvas.pack()
-
 
 def create_circle(x, y, r, canvas_name, fill, tag):  # center coordinates, radius
     x0 = x - r
@@ -19,113 +15,45 @@ def create_circle(x, y, r, canvas_name, fill, tag):  # center coordinates, radiu
     y1 = y + r
     return canvas_name.create_oval(x0, y0, x1, y1, fill=fill, tags=tag)
 
+def load_new_game():
+    premier_league = load_league("Premier League")
+    premier_league_clubs = premier_league.clubs
+    teams = load_teams(premier_league_clubs[0].name, premier_league_clubs[1].name)
+    team1 = teams[0]
+    team2 = teams[1]
 
-def graphics():
-    team1_player_tag_dict = {}
-    team2_player_tag_dict = {}
+    team1_squad = team1.player_list_for_game()[0]
+    team1_rating = team1.player_list_for_game()[1]
+    team1_formation = team1.player_list_for_game()[2]
 
-    counter1 = 1
-    for pl in game.team1.best_squad:
-        team1_player_tag_dict[pl.name] = f"team1player{counter1}"
-        counter1 += 1
-    counter2 = 1
-    for pl in game.team2.best_squad:
-        team2_player_tag_dict[pl.name] = f"team2player{counter2}"
+    team2_squad = team2.player_list_for_game()[0]
+    team2_rating = team2.player_list_for_game()[1]
+    team2_formation = team2.player_list_for_game()[2]
+    game = Game(team1, team2)
 
-    game.reset_origin_positions()
-    while game.minute <= 90:
-        # game.set_attacking_defending_positions()
-        counter1 = 1
-        counter2 = 1
-        #     # First we need to remove the circles
-        for i in range(1, 3):
-            for j in range(1, 12):
-                myCanvas.delete(f"team{i}player{j}")
-        myCanvas.delete("ball")
-        for pl in game.team1.best_squad:
-            current_position_x = pl.pos_x
-            current_position_y = pl.pos_y
-            create_circle(5 * current_position_x, 8 * (50 - current_position_y), 10, myCanvas, "lightblue",
-                          team1_player_tag_dict[pl.name])
-            counter1 += 1
-        for pl in game.team2.best_squad:
-            current_position_x = pl.pos_x
-            current_position_y = pl.pos_y
-            create_circle(5 * current_position_x, 8 * (50 - current_position_y), 10, myCanvas, "red",
-                          team2_player_tag_dict[pl.name])
-        create_circle(5 * game.ball_pos_x, 8 * (50 - game.ball_pos_y), 5, myCanvas, "white", "ball")
-        tk.update()
-        time.sleep(0.1)
+    print(team1.name)
+    print(f"Rating:{team1_rating}")
+    print(f"Formation:{team1_formation}")
+    print("---------------------------------")
+    for player in team1_squad:
+        print(player.position + "   " + player.name + "    " + f"{[player.pos_x, player.pos_y]}")
+    print("\n\n\n")
 
+    print(team2.name)
+    print(f"Rating:{team2_rating}")
+    print(f"Formation:{team2_formation}")
+    print("----------------------------------")
+    for pl in team2_squad:
+        print(pl.position + "  " + pl.name + "    " + f"{[pl.pos_x, pl.pos_y]}")
+    print("\n\n\n")
+    print("Game starts!")
 
-team1_squad = team1.player_list_for_game()[0]
-team1_rating = team1.player_list_for_game()[1]
-team1_formation = team1.player_list_for_game()[2]
-
-team2_squad = team2.player_list_for_game()[0]
-team2_rating = team2.player_list_for_game()[1]
-team2_formation = team2.player_list_for_game()[2]
-game = Game(team1, team2)
-
-print(team1.name)
-print(f"Rating:{team1_rating}")
-print(f"Formation:{team1_formation}")
-print("---------------------------------")
-for player in team1_squad:
-    print(player.position + "   " + player.name + "    " + f"{[player.pos_x, player.pos_y]}")
-print("\n\n\n")
-
-print(team2.name)
-print(f"Rating:{team2_rating}")
-print(f"Formation:{team2_formation}")
-print("----------------------------------")
-for pl in team2_squad:
-    print(pl.position + "  " + pl.name + "    " + f"{[pl.pos_x, pl.pos_y]}")
-print("\n\n\n")
-print("Game starts!")
-
-team1_player_tag_dict = {}
-team2_player_tag_dict = {}
-
-counter1 = 1
-for pl in game.team1.best_squad:
-    team1_player_tag_dict[pl.name] = f"team1player{counter1}"
-    counter1 += 1
-counter2 = 1
-for pl in game.team2.best_squad:
-    team2_player_tag_dict[pl.name] = f"team2player{counter2}"
-
-
-def graphics():
-    # game.set_attacking_defending_positions()
-    counter1 = 1
-    counter2 = 1
-
-    #     # First we need to remove the circles
-    for i in range(1, 3):
-        for j in range(1, 12):
-            myCanvas.delete(f"team{i}player{j}")
-    myCanvas.delete("ball")
-    for pl in game.team1.best_squad:
-        current_position_x = pl.pos_x
-        current_position_y = pl.pos_y
-        create_circle(5 * current_position_x, 8 * (50 - current_position_y), 10, myCanvas, "lightblue",
-                      team1_player_tag_dict[pl.name])
-        counter1 += 1
-    for pl in game.team2.best_squad:
-        current_position_x = pl.pos_x
-        current_position_y = pl.pos_y
-        create_circle(5 * current_position_x, 8 * (50 - current_position_y), 10, myCanvas, "red",
-                      team2_player_tag_dict[pl.name])
-    create_circle(5 * game.ball_pos_x, 8 * (50 - game.ball_pos_y), 5, myCanvas, "white", "ball")
-    tk.update()
-    time.sleep(0.1)
+    return game
 
 def run_game(game):
     game.reset_origin_positions()
     while game.minute <= 90:
         # graphics()
-        time.sleep(0.1)
         game.play()
     winning_side = game.team1.name
     if game.team1_score < game.team2_score:
@@ -146,6 +74,10 @@ def run_game(game):
 
     myCanvas.mainloop()
 def main():
+    game = load_new_game()
+    print(game.team1.name)
+    print(game.team1.best_squad)
+    print(game.team1.formation)
     run_game(game)
 
 
