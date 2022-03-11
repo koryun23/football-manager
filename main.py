b0e1561c687d6795924f577735d36a088ec40904
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-11 23:30:31 +0400

    simulating the whole league without using multithreading(still need to improve the underlying game logic because lots of unexpected results happen)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from threading import Thread
 
 from game import Game
+from game_data import GameData
 from load_data import *
 
 
@@ -46,61 +47,58 @@ def load_new_game(team1_name, team2_name, tournament_name):
 
     return game
 
-
-results = []
+list_of_game_data = []
 def run_game(game):
     game.reset_origin_positions()
     while game.minute <= 90:
         # graphics()
         game.play()
-
-    results.append((game.team1_score, game.team2_score))
+    list_of_game_data.append(GameData(game))
 
 def main():
     epl = load_league("Premier League")
-    pairings_for_first_round = [pairing for pairing in epl.pairings[0]]
-    threads = []
-    for pair in pairings_for_first_round:
-        game = load_new_game(pair[0].name, pair[1].name, epl.name)
-        threads.append(Thread(target=run_game, args=(game,)))
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
 
-    for thread in threads:
-        if not thread.is_alive():
-            thread.handled = True
+    # main loop
+    for j in range(len(epl.pairings) * 2):
 
-    for i in range(len(results)):
-        print(i, len(pairings_for_first_round))
-        row = epl.get_row_by_club_name(pairings_for_first_round[i][0])
-        row.set_goals_scored(row.get_goals_scored() + results[i][0])
-        row.set_goals_conceded(row.get_goals_conceded() + results[i][1])
-        row.set_matches_played(row.get_matches_played() + 1)
-        if results[i][0] > results[i][1]: row.set_matches_won(row.get_matches_won() + 1)
-        elif results[i][0] < results[i][1]: row.set_matches_lost(row.get_matches_lost() + 1)
-        else: row.set_matches_drawn(row.get_matches_drawn() + 1)
-        row = epl.get_row_by_club_name(pairings_for_first_round[i][1])
-        row.set_goals_scored(results[i][1])
-        row.set_goals_conceded(results[i][0])
-        row.set_matches_played(row.get_matches_played() + 1)
-        if results[i][0] < results[i][1]: row.set_matches_won(row.get_matches_won() + 1)
-        elif results[i][0] > results[i][1]: row.set_matches_lost(row.get_matches_lost() + 1)
-        else: row.set_matches_drawn(row.get_matches_drawn() + 1)
-        winning_side = None
-        if results[i][0] > results[i][1]:
-            winning_side = pairings_for_first_round[i][0]
-        elif results[i][0] < results[i][1]:
-            winning_side = pairings_for_first_round[i][1]
-        if winning_side:
-            for row in epl.standings:
-                if winning_side == row.get_club():
-                    row.set_points(row.get_points() + 3)
-        else:
-            for row in epl.standings:
-                if pairings_for_first_round[i][0] == row.get_club() or pairings_for_first_round[i][1] == row.get_club():
-                    row.set_points(row.get_points() + 1)
+        results = []
+        pairings_for_first_round = epl.pairings[0]
+        for pair in pairings_for_first_round:
+            game = load_new_game(pair[0].name, pair[1].name, epl.name)
+            run_game(game)
+            data_for_current_game = GameData(game)
+            list_of_game_data.append(data_for_current_game)
+            results.append((game.team1_score, game.team2_score))
+
+        for i in range(len(results)):
+            print(i, len(pairings_for_first_round))
+            row = epl.get_row_by_club_name(pairings_for_first_round[i][0])
+            row.set_goals_scored(row.get_goals_scored() + results[i][0])
+            row.set_goals_conceded(row.get_goals_conceded() + results[i][1])
+            row.set_matches_played(row.get_matches_played() + 1)
+            if results[i][0] > results[i][1]: row.set_matches_won(row.get_matches_won() + 1)
+            elif results[i][0] < results[i][1]: row.set_matches_lost(row.get_matches_lost() + 1)
+            else: row.set_matches_drawn(row.get_matches_drawn() + 1)
+            row = epl.get_row_by_club_name(pairings_for_first_round[i][1])
+            row.set_goals_scored(results[i][1])
+            row.set_goals_conceded(results[i][0])
+            row.set_matches_played(row.get_matches_played() + 1)
+            if results[i][0] < results[i][1]: row.set_matches_won(row.get_matches_won() + 1)
+            elif results[i][0] > results[i][1]: row.set_matches_lost(row.get_matches_lost() + 1)
+            else: row.set_matches_drawn(row.get_matches_drawn() + 1)
+            winning_side = None
+            if results[i][0] > results[i][1]:
+                winning_side = pairings_for_first_round[i][0]
+            elif results[i][0] < results[i][1]:
+                winning_side = pairings_for_first_round[i][1]
+            if winning_side:
+                for row in epl.standings:
+                    if winning_side == row.get_club():
+                        row.set_points(row.get_points() + 3)
+            else:
+                for row in epl.standings:
+                    if pairings_for_first_round[i][0] == row.get_club() or pairings_for_first_round[i][1] == row.get_club():
+                        row.set_points(row.get_points() + 1)
     epl.standings.sort(key=lambda standing_row: (-standing_row.get_points(), -standing_row.get_goals_scored(), -standing_row.get_goals_difference()))
     print("-------------------------------------------------------------")
     epl.print_standings()
