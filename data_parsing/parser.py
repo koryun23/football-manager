0a8856fffff26073e880e5ee48e0d6961b26ef4e
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-15 18:17:17 +0400

    parsing the links of players of all the teams of the requested league

--- data_parsing/parser.py
+++ data_parsing/parser.py
@@ -22,10 +22,15 @@ def parse_teams(tournament_name):
 def parse_players(tournament_name):
     teams = parse_teams(tournament_name)
     for team in teams:
-        print(team.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
-        soup = get_soup(team.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
+        players = parse_players_of_team(team)
+        for player in players:
+            print(player.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
 
-
-
+def parse_players_of_team(team):
+    soup = get_soup(team.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
+    main_table = soup.find("table", {"class": "table"})
+    main_table_body = main_table.find("tbody")
+    main_table_rows = main_table_body.find_all(lambda tag: tag.name == "tr" and len(tag.findChildren("td", recursive=False)) > 1)
+    return main_table_rows
 
 parse_players("english-premier-league")
