8fbceabd020d73c13a89726966130131f667af53
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-15 21:28:29 +0400

    parsing all the primary information about the players(will probably take a long time to parse data for all the players)

--- data_parsing/parser.py
+++ data_parsing/parser.py
@@ -22,6 +22,8 @@ def parse_teams(tournament_name):
 def parse_players(tournament_name):
     teams = parse_teams(tournament_name)
     for team in teams:
+        current_team = team
+        current_team_name = current_team.find("span", {"class": "entry-font"}).find("a").text
         players = parse_players_of_team(team)
         parse_single_player_of_team(players)
 
@@ -36,7 +38,18 @@ def parse_players_of_team(team):
 
 def parse_single_player_of_team(players):
     for player in players:
-        print(player.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
+        soup = get_soup(player.find("span", {"class": "entry-font"}).find("a", href=True)['href'])
+        player_info = soup.find("div", {"class": "player-info"})
+        name = player_info.find("h1").text.strip()
+
+        other_info = player_info.find("div", {"class": "header-subtitle"}).find_all("p", {"class": "mb-0"})
+        nationality = other_info[0].find("a").text.strip()
+        team = other_info[1].find("a").text.strip()
+        kit_number = other_info[2].text[other_info[2].text.index("#"):]
+        best_foot = other_info[6].text[other_info[6].text.index(": ")+2:]
+        print(f"Name: {name}\nNationality: {nationality}\nTeam: {team}\nKit Number: {kit_number}\nBest Foot: {best_foot}")
+        print("------------------------------------------")
+
 
 
 parse_players("english-premier-league")
